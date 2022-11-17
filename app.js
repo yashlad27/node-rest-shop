fc19f98f71c1327a548e9448fe4f4dcd9c7fbe87
Yash Lad <yashlad727@gmail.com>
2022-11-17 23:33:59 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const productRoutes = require('./api/routes/products');
+
+// app.use((req, res, next) => {
+//     res.status(200).json({
+//         message: "It works!"
+//     });
+// });  
+
+app.use('/products', productRoutes);
+
+module.exports = app;
